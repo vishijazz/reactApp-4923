87a3b936e364f9860a4512374639da64c7f1ec08
Vishal_SR <52326540+Vishal-SR-505@users.noreply.github.com>
2024-03-11 15:19:42 +0530

    Delete foodApp directory

--- foodApp/src/utils/constants.js
+++
@@ -1,6 +0,0 @@
-export const CDN_URL =
-  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
-export const FETCH_ALL_ITEMS =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.996424660943092&lng=80.19526943564414&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
-export const FETCH_RESTAURANT_MENU =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.996424660943092&lng=80.19526943564414&restaurantId=";
